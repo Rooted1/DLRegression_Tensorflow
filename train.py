1bb4630d81fd8aff8ddbd326f086e0d71dc096ff
Rooted1 <ruthobey@gmail.com>
2023-06-15 18:15:13 -0500

    split features and labels each into train and test sets

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 ####### DATA PREPROCESSING
 
@@ -9,3 +10,8 @@ dataset = dataset.drop(['Serial No.'], axis=1)
 # split data into features and labels parameters
 features = dataset.iloc[:, 0:-1]
 labels = dataset.iloc[:, -1] # column to predict
+
+# split features and labels each into training and test sets
+X_train, X_tests, y_train, y_test = train_test_split(features, labels, test_size=0.35, random_state=42)
+
+print(y_test)
