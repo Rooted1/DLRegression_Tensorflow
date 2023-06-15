9ee0608912af66c8965283c3f3d78d7531459b80
Rooted1 <ruthobey@gmail.com>
2023-06-15 17:45:37 -0500

    drop unwanted column and split data into features set and labels set

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+####### DATA PREPROCESSING
+
 # load dataset as DataFrame
 dataset = pd.read_csv('admissions_data.csv')
+dataset = dataset.drop(['Serial No.'], axis=1)
 
-print(dataset)
+# split data into features and labels parameters
+features = dataset.iloc[:, 0:-1]
+labels = dataset.iloc[:, -1] # column to predict
